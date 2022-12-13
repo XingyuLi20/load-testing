8fa8f1b4a1fef04cce492672441082024c6fffab
XingyuLi20 <lixingyu@berkeley.edu>
2022-12-13 15:25:41 -0800

    add readme & sleep

---
+++ README.md
@@ -0,0 +1,10 @@
+Install [k6](https://k6.io/docs/get-started/installation/) MacOS
+```
+brew install k6
+```
+
+
+Testing
+```
+k6 run sample_test.js
+```
--- sample_test.js
+++ sample_test.js
@@ -1,4 +1,5 @@
 import http from 'k6/http';
+import { sleep } from 'k6';
 
 export let options = {
     insecureSkipTLSVerify: true,
@@ -9,4 +10,5 @@ export let options = {
 
 export default () => {
     http.get('http://localhost:8088/cedar/v1/offer/collection/0xed5af388653567af2f388e6224dc7c4b3241c544');
+    sleep(1);
 };
